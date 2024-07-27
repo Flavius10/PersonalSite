1df85dd46d6cb5a6ae8007728fb141e17524205a
Alexa Flavius <flaviusa1010@gmail.com>
2024-07-27 19:32:46 +0300

    Footer modified

---
+++ src/Pages/ContactPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Stylesheets/GeneralStyleSheets.css'
+
+const SkillsPage = () => {
+  return (
+    <div className="body">SkillsPage</div>
+  )
+}
+
+export default SkillsPage
---
+++ src/Pages/PortofolioPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Stylesheets/GeneralStyleSheets.css'
+
+const ProjectsPage = () => {
+  return (
+    <div className="body">Projects</div>
+  );
+}
+
+export default ProjectsPage
