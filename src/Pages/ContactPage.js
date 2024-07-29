9e0ddf90fabb5e0647de6da547bfbc5d1d84c062
Alexa Flavius <flaviusa1010@gmail.com>
2024-07-29 21:07:07 +0300

    ContactPage

--- src/Pages/ContactPage.js
+++ src/Pages/ContactPage.js
@@ -1,10 +1,55 @@
-import React from 'react'
-import './Stylesheets/GeneralStyleSheets.css'
+import React from 'react';
+import { Box, TextField, Button} from '@mui/material';
+import { Typography } from '@mui/material';
 
-const SkillsPage = () => {
+const ContactPage = () => {
   return (
-    <div className="body">SkillsPage</div>
-  )
+    <div className="body">
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          height: '60vh',
+          width: '100vw', 
+          gap: 5,
+        }}
+      >
+
+        <Typography sx={{fontSize:30, 
+          fontFamily:'monospace'
+        }}>Contact
+        </Typography>
+
+        <TextField
+          id="outlined-basic-1"
+          label="Nume"
+          variant="outlined"
+          sx={{ width: '40vw' }}
+        />
+        <TextField
+          id="outlined-basic-2"
+          label="Email"
+          variant="outlined"
+          sx={{ width: '40vw' }}
+        />
+        <TextField
+          id="outlined-basic-2"
+          label="Email"
+          variant="outlined"
+          sx={{ width: '40vw' }}
+        />
+
+      <Button variant="contained"
+              sx={{width:100, height: 50}}>
+        Submit
+      </Button>
+
+      </Box>
+
+    </div>
+  );
 }
 
-export default SkillsPage
+export default ContactPage;
