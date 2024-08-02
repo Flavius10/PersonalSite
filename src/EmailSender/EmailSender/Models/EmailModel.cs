c698e3a0c609cee928398ffbc8fec950fa6dd450
Alexa Flavius <flaviusa1010@gmail.com>
2024-08-02 15:10:37 +0300

    Email Sender schimbat

--- src/EmailSender/EmailSender/Models/EmailModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EmailSender.Models
-{
-    public class EmailModel
-    {
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string Message { get; set; }
-    }
-}
